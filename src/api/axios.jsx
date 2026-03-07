cc74b6a4122c18b95da9df541bc661b69bb47962
Hary Chandra Mahardika <chandramahardika82@gmail.com>
2026-03-07 09:51:13 +0700

    updated bug

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import toast from "react-hot-toast"
 
 const api = axios.create({
-  baseURL: "https://wallet.domain-saya.my.id/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://wallet.domain-saya.my.id/api",
   headers: {
     Accept: "application/json"
   }
